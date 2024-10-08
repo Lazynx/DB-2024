2e376db046ce15b85dfcf926160d67eb9ec54145
Lazynx <frichaplay@gmail.com>
2024-10-08 18:07:33 +0500

    [fix]: some changes

--- lab4.sql
+++ lab4.sql
@@ -1,11 +1,7 @@
-
--- 1. Создание базы данных «lab4»
+-- 1. Create database called «lab4»
 CREATE DATABASE lab4;
 
--- Переключаемся на базу данных lab4
-\c lab4;
-
--- 2. Создание таблиц «Warehouses» и «Boxes»
+-- 2. Create following tables «Warehouses» and «Boxes»:
 
 CREATE TABLE Warehouses (
     code INTEGER PRIMARY KEY,
@@ -21,7 +17,9 @@ CREATE TABLE Boxes (
     FOREIGN KEY (warehouse) REFERENCES Warehouses(code)
 );
 
--- Вставка данных в таблицу Warehouses
+-- Download lab4.sql file from intranet. Copy content of the file
+-- and run using Query Tool (check if tables created)
+
 INSERT INTO Warehouses (code, location, capacity) VALUES
 (1, 'Chicago', 3),
 (2, 'Chicago', 4),
@@ -29,7 +27,6 @@ INSERT INTO Warehouses (code, location, capacity) VALUES
 (4, 'Los Angeles', 2),
 (5, 'San Francisco', 8);
 
--- Вставка данных в таблицу Boxes
 INSERT INTO Boxes (code, contents, value, warehouse) VALUES
 ('0MN7', 'Rocks', 180, 3),
 ('4HBP', 'Rocks', 250, 1),
@@ -43,43 +40,48 @@ INSERT INTO Boxes (code, contents, value, warehouse) VALUES
 ('P2T6', 'Scissors', 150, 2),
 ('TU55', 'Papers', 90, 5);
 
--- 3. Запрос всех складов
+-- 4. Select all warehouses with all columns.
 SELECT * FROM Warehouses;
 
--- 4. Запрос всех коробок, у которых значение больше 150
+-- 5. Select all boxes with a value larger than $150.
 SELECT * FROM Boxes WHERE value > 150;
 
--- 5. Запрос всех уникальных содержимых коробок
+-- 6. Select all the boxes distinct by contents.
 SELECT DISTINCT contents FROM Boxes;
 
--- 6. Запрос кода склада и количества коробок на каждом складе
+-- 7. Select the warehouse code and the number of the boxes in
+-- each warehouse.
 SELECT warehouse, COUNT(*) AS number_of_boxes
 FROM Boxes
 GROUP BY warehouse;
 
--- 7. Запрос тех складов, где количество коробок больше 2
+-- 8. Same as previous exercise, but select only those warehouses
+-- where the number of the boxes is greater than 2.
 SELECT warehouse, COUNT(*) AS number_of_boxes
 FROM Boxes
 GROUP BY warehouse
 HAVING COUNT(*) > 2;
 
--- 8. Создание нового склада в Нью-Йорке с вместимостью 3 коробки
+-- 9. Create a new warehouse in New York with a capacity for 3
+-- boxes.
 INSERT INTO Warehouses (code, location, capacity) VALUES
 (6, 'New York', 3);
 
--- 9. Создание новой коробки с кодом «H5RT», содержащей «Papers», стоимостью $200 и расположенной на складе 2
+-- 10. Create a new box, with code "H5RT", containing "Papers" with
+-- a value of $200, and located in warehouse 2.
 INSERT INTO Boxes (code, contents, value, warehouse) VALUES
 ('H5RT', 'Papers', 200, 2);
 
--- 10. Уменьшение стоимости третьей по величине коробки на 15%
+-- 11. Reduce the value of the third largest box by 15%.
 UPDATE Boxes
 SET value = value * 0.85
 WHERE code = (SELECT code FROM Boxes ORDER BY value DESC LIMIT 1 OFFSET 2);
 
--- 11. Удаление всех коробок со стоимостью меньше 150
+-- 12. Remove all boxes with a value lower than $150.
 DELETE FROM Boxes WHERE value < 150;
 
--- 12. Удаление всех коробок, расположенных в Нью-Йорке, с возвратом всех удаленных данных
+-- 13. Remove all boxes which is located in New York. Statement
+-- should return all deleted data.
 DELETE FROM Boxes
 WHERE warehouse = (SELECT code FROM Warehouses WHERE location = 'New York')
 RETURNING *;
